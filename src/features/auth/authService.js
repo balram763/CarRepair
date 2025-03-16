fd1975a385244339e546173c4a325b4cbed463e2
balram <85399069+balram763@users.noreply.github.com>
2025-03-17 03:18:01 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const register = async (formData) => {
-  const response = await axios.post("/api/user", formData);
+  const response = await axios.post("https://carrepairbackend.onrender.com/api/user", formData);
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 };
 
 
 const login = async (formData) => {
-    const response = await axios.post("/api/user/login", formData)
+    const response = await axios.post("https://carrepairbackend.onrender.com/api/user/login", formData)
     localStorage.setItem("user", JSON.stringify(response.data));
     return response.data;
 
